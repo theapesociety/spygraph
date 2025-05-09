752ac90290a58bff8a16284712969934f1bcd480
theapesociety <team@theapesociety.io>
2025-05-08 22:32:25 -0700

    set network

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,32 +4,33 @@ import { http } from "viem";
 import { SpyGameABI } from "./abis/SpyGameABI";
 import { SpyNFTABI } from "./abis/SpyNFTABI";
 import { SpyIntelABI } from "./abis/SpyIntelABI";
+import { baseSepolia } from "viem/chains";
 
 export default createConfig({
   networks: {
-    mainnet: {
-      chainId: 1,
+    baseSepolia: {
+      chainId: 84532,
       transport: http(process.env.PONDER_RPC_URL_1),
     },
   },
   contracts: {
     SpyGameABI: {
-      network: "mainnet",
+      network: "baseSepolia",
       abi: SpyGameABI,
-      address: "0x0000000000000000000000000000000000000000",
-      startBlock: 1234567,
+      address: "0x2beda07368356637ebc639983723ab2958f4b100",
+      startBlock: 25390000,
     },
     SpyNFTABI: {
-      network: "mainnet",
+      network: "baseSepolia",
       abi: SpyNFTABI,
-      address: "0x0000000000000000000000000000000000000000",
-      startBlock: 1234567,
+      address: "0x182c7db3e13513110be570a0c4fddfb752cc7d18",
+      startBlock: 25390000,
     },
     SpyIntelABI: {
-      network: "mainnet",
+      network: "baseSepolia",
       abi: SpyIntelABI,
-      address: "0x0000000000000000000000000000000000000000",
-      startBlock: 1234567,
+      address: "0xb2b3625327cc36acfeb8fe01c58277eafd6f7e1b",
+      startBlock: 25390000,
     },
   },
 });
